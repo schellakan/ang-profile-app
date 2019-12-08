780acca36d3edc266c175eaa0d0a81bffb4a286d
Sam Chellakan <sam_chellakan@hotmail.com>
2019-12-07 20:39:10 -0800

    changes

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/about.JPG)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Angelin Chellakan</a></h1>
               <span className="email"><i className="icon-mail"></i> angelin_shoba@hotmail.com</span>
             </div>
