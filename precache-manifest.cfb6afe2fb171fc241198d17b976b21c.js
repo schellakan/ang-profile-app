649bfada57ea45f86cdf652a8eb9148203215305
Sam Chellakan <sam_chellakan@hotmail.com>
2019-12-07 20:06:59 -0800

    Updates

---
+++ precache-manifest.cfb6afe2fb171fc241198d17b976b21c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec7234a5d5e86073c71c",
+    "url": "/ang-profile-app/static/css/main.20f8c76c.chunk.css"
+  },
+  {
+    "revision": "ec7234a5d5e86073c71c",
+    "url": "/ang-profile-app/static/js/main.ec7234a5.chunk.js"
+  },
+  {
+    "revision": "bae2cb1f97633ef72686",
+    "url": "/ang-profile-app/static/js/runtime~main.bae2cb1f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/ang-profile-app/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d2964c57a720cdbf66724018ea6ffff4",
+    "url": "/ang-profile-app/index.html"
+  }
+];
